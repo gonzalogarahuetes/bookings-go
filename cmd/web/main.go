80d5a7a18858d2a4c27d1bb3e4d7b0c6ec04cf38
Gonzalo García Arahuetes <gonzaloarahuetes@gmail.com>
2024-11-11 16:00:36 +0100

    improve error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/gonzalogarahuetes/bookings-go/internal/config"
 	"github.com/gonzalogarahuetes/bookings-go/internal/handlers"
+	"github.com/gonzalogarahuetes/bookings-go/internal/helpers"
 	"github.com/gonzalogarahuetes/bookings-go/internal/models"
 	"github.com/gonzalogarahuetes/bookings-go/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -42,6 +46,12 @@ func run() error {
 
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -62,6 +72,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
