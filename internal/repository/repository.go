ad4974b1376a8a493b37f8fd924426b873612444
Gonzalo García Arahuetes <gonzaloarahuetes@gmail.com>
2024-12-04 15:58:03 +0100

    create insert reservation function

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,8 @@
 package repository
 
+import "github.com/gonzalogarahuetes/bookings-go/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) error
 }
